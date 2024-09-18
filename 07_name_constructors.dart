c2c2bd909c357d9d6aa50dae653558d3cd114c2c
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-18 14:20:51 -0600

    Name constructors to construct object based on certain types. In this case the type is Map<String,dynamic>

---
+++ 07_name_constructors.dart
@@ -0,0 +1,45 @@
+void main() {
+  
+  final Map<String, dynamic> rawJson = {
+    'name': 'Tony Stark',
+    'power': 'Money',
+    'isAlive': true
+  };
+    
+final ironman = Hero.fromJson( rawJson );
+  
+//   final ironman = Hero(
+//     isAlive: false,
+//     power: 'Money',
+//     name: 'Tony Stark'
+//   );
+
+  print( ironman );
+}
+
+class Hero {
+  
+  String name;
+  String power;
+  bool isAlive;
+  
+  Hero({
+    required this.name,
+    required this.power,
+    required this.isAlive
+  });
+  
+  // constructor for constructing object from json format
+  Hero.fromJson( Map<String,dynamic> json ) 
+     : name = json['name'] ?? 'No name found!',
+       power = json['power'] ?? 'No power found!',
+       isAlive = json['isAlive'] ?? 'No isAlive found!';
+  
+
+  
+  @override
+  String toString() {
+    return '$name, $power, isAlive: ${ isAlive ? 'Yes':'No' }';
+  }
+
+}
