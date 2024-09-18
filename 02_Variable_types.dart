d72764279cd4cf2899aa23bbdbe6784fae744635
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-18 12:05:12 -0600

    Dynamic type

--- 02_Variable_types.dart
+++ 02_Variable_types.dart
@@ -1,4 +1,5 @@
 void main() {
+  // Final means the value can't be changed (runtime)
   final String pokemon = "Aegislash";
   final int hp = 100;
   final bool isAlive = true;
@@ -8,11 +9,20 @@ void main() {
     "Aegislash/swordForm.png"
   ];
 
+  // Can be changed without restrictions
+  dynamic errorMessage = "Hola";
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5];
+  errorMessage = (1, 2, 3, 4, 5);
+  errorMessage = () => true;
+  errorMessage = null;
+
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   """);
 }
