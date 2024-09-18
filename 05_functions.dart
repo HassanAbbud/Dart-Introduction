1d812342d8b3bf08c1761d61f94e960fffeb1a79
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-18 13:34:30 -0600

    Named parameters in functions

--- 05_functions.dart
+++ 05_functions.dart
@@ -3,6 +3,8 @@ void main() {
   print( greetEveryone() );
   
   print( 'Addition: ${ addTwoNumbers( 10, 20 ) }' );
+
+    print( greetPerson( name: 'Hassan', message: 'Hi,' ) );
 }
 
 
@@ -10,8 +12,14 @@ String greetEveryone() => 'Hello everyone, this is my function!';
 
 int addTwoNumbers( int a, int b ) => a + b;
 
-// function with optional argument
+// function with optional parameter
 int addTwoNumbersOptional( int a, [ int b = 0 ]) {
 //   b ??= 0; if b is null then set to 0
   return a + b;
+}
+
+// function with named. When calling the function, parameters don't need to be called by position 
+String greetPerson({ required String name, String message = 'Hola,' }) {
+  
+  return '$message $name';
 }
