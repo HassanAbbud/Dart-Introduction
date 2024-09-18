6c7692dc5f822e55a22e2ba251f3d813a51d2b6d
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-18 13:59:51 -0600

    @override & named properties in constructor of Class

--- 05_functions.dart
+++ 05_functions.dart
@@ -18,7 +18,7 @@ int addTwoNumbersOptional( int a, [ int b = 0 ]) {
   return a + b;
 }
 
-// function with named. When calling the function, parameters don't need to be called by position 
+// function with named parameter. When calling the function, parameters don't need to be called by position 
 String greetPerson({ required String name, String message = 'Hola,' }) {
   
   return '$message $name';
