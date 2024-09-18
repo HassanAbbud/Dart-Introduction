a770d8c4943de5b677dde9c10b7786aefdedef01
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-18 17:45:20 -0600

    Extends class in Dart

--- 09_abstract_classes.dart
+++ 09_abstract_classes.dart
@@ -1,9 +1,18 @@
 void main(){
-
+  final windPlant = WindPlant( initialEnergy: 100 );
+  
+  print( 'wind: ${ chargePhone( windPlant ) }' );
 }
 
 enum PlantType { nuclear, wind, water }
 
+double chargePhone( EnergyPlant plant ) {
+  if ( plant.energyLeft < 10 ) {
+    throw Exception('Not enough energy');
+  }
+    
+  return plant.energyLeft - 10;
+}
 
 abstract class EnergyPlant {
   
@@ -17,4 +26,15 @@ abstract class EnergyPlant {
   
   void consumeEnergy( double amount );
   
+}
+
+class WindPlant extends EnergyPlant {
+  
+  WindPlant({ required double initialEnergy })
+      : super( energyLeft: initialEnergy, type: PlantType.wind );
+  
+  @override
+  void consumeEnergy( double amount ) {
+    energyLeft -= amount;
+  }
 }
